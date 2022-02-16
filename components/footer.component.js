933f118b2703f4bb6394da74df3cc9545e1dc090
Mauricio <mauricio-aguirre2001@hotmail.com>
2022-02-15 21:19:43 -0500

    creacion componentes y avance pag inicio

---
+++ components/footer.component.js
@@ -0,0 +1,24 @@
+const Footer = () => {
+    return <footer className="card mt-4">
+    <div className="card-body">
+        <div className="row">
+            <div className="col-sm-6">
+                <ul>
+                    <li> <a href="">Nosotros</a></li>
+                    <li> <a href="">Terminos y condicioes</a></li>
+                </ul>
+            </div>
+            <div className="col-sm-6">
+                <ul>
+                    <li>Grupo ...</li>
+                    <li>ULIMA-2020</li>
+                </ul>
+            </div>
+            
+        </div>
+    </div>
+ </footer>
+
+}
+
+export default Footer
