44b9d3e97971e136e4887610a9e295f305386543
Mauricio <mauricio-aguirre2001@hotmail.com>
2022-02-16 14:32:06 -0500

    avance index y creacion pag nosotros

---
+++ pages/nosotros.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Footer from "../components/footer.component";
+import MenuNavegacion from "../components/menuNavegacion.component";
+
+function Nosotros() {
+    return ( <div>
+            <MenuNavegacion/>
+            
+            <Footer/>
+
+    </div>
+
+    )
+}
+
+export default Nosotros
