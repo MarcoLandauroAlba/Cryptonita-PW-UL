2764753241ed3d450f8c475dfdc2a17c303ac893
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2022-02-17 12:28:22 -0500

    componentizando pantallas
    por favor sigan componentizandolas

--- pages/TerminosYCondiciones.js
+++ pages/TerminosYCondiciones.js
@@ -1,42 +1,12 @@
 import { Image } from "react-bootstrap";
+import BloqueTermYCond from "../components/BloqueTermYCond.components";
 import Footer from "../components/footer.component";
 import MenuNavegacion from "../components/menuNavegacion.component";
 
 export default function TerminosyCondicionesPage() {
-    return <div className="row">
-        <MenuNavegacion/>
-        <h2 className="mt-4">Términos y Condiciones</h2>
-        <div className="col-md-8 mt-4">
-            <ul>
-                <li><h5>Información del sitio</h5></li>
-                <p>
-                    Sitio de cambio de criptomonedas en la región peruana
-                    que busca agilizar la labor de los cambistas para establecer
-                    el cambio de manera online. Página que es realizada para un
-                    proyecto universitario y no está avalada como tal por un banco
-                    o entidad financiera.
-                </p>
-                <li><h5>Usuarios</h5></li>
-                <p>
-                    La página se compromete en la protección de los datos personales
-                    y la no divulgación de algún dato sensible que pueda afectar directa
-                    o indirectamente a alguno de nuestros usuarios
-                </p>
-                <li><h5>Regulaciones</h5></li>
-                <p>
-                    De acuerdo a las siguientes leyes: ...
-                </p>
-            </ul>
-        </div>
-        <div className="col-md-4 mt-4">
-            <div className="d-grid gap-2 col-6 mx-auto">
-                <button className="btn btn-primary" onClick={() => {
-                    location.href = "/"
-                }} type="button">Regresar</button>
-            </div>
-            <Image src="https://www.esan.edu.pe/images/blog/2018/01/30/1500x844-terminos-condiciones.jpg" thumbnail={true}
-                className="mt-4"></Image>
-        </div>
-        <Footer></Footer>
+    return <div>
+        <MenuNavegacion />
+        <BloqueTermYCond />
+        <Footer />
     </div>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,51 @@
 import MenuNavegacion from '../components/menuNavegacion.component'
 import Footer from '../components/footer.component'
 import Carrusel from "../components/carrusel.component"
+import MenuNavegacionusuarioregistrado from '../components/menuNavegacionClienteRegistrado.component'
+import { useEffect, useState } from 'react'
+import MenuNavegacionadminregistrado from '../components/menuNavegacionAdminRegistrado.component'
 export default function Home() {
-    return (
-        <div>
-            <MenuNavegacion />
-            <Carrusel />
-            <Footer />
-        </div>
-    )
+    // SI USUARIO NO ESTA LOGEADO
+    const [tipoUsuario, setTipoUsuario] = useState("NoLog")
+    // tipos de usuarios:
+        //  NoLog -> No logeado
+        //  LoggedNC -> Usuario logeado no confirmado
+        //  LoggedC -> Usuario logeado confirmado
+        //  Admin
+
+    if(tipoUsuario=="NoLog"){
+        return (
+            <div>
+                <MenuNavegacion/>
+                <Carrusel />
+                <Footer />
+            </div>
+        )
+    }else if(tipoUsuario=="LoggedNC"){
+        return (
+            <div>
+                <MenuNavegacionusuarioregistrado/>
+                <Carrusel />
+                <Footer />
+            </div>
+        )
+    }else if(tipoUsuario=="LoggedC"){
+        return (
+            <div>
+                <MenuNavegacionadminregistrado />
+                <Carrusel />
+                <Footer />
+            </div>
+        )
+    }else if(tipoUsuario=="Admin"){
+        return (
+            <div>
+                <MenuNavegacionLogConf />
+                <Carrusel />
+                <Footer />
+            </div>
+        )
+    }
+    
+    
 }
