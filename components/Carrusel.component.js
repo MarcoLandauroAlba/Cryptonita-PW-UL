0a408fd006d5ed2ae08a11263e26b9e971c1bc25
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2022-02-17 10:25:41 -0500

    igualar

--- components/Carrusel.component.js
+++ components/Carrusel.component.js
@@ -1,31 +1,40 @@
 const Carrusel = () => {
     return (<div><h1 className='mt-4 text-center'>BITCOIN AL DÍA</h1>
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
+                    {/* PROPUESTAS */}
                     <div className="card bg-dark text-white mt-4 ">
-                        <img className='card-img-top' height={700} src='https://estaticos-cdn.elperiodico.com/clip/5d0a3ba8-43db-4b2f-abc9-882d8344e5d5_alta-libre-aspect-ratio_default_0.jpg' alt='Aqui va una imagen'></img>
-                        <div className="card-img-overlay">
-                            <div className='m-5'>
-                                <div>&nbsp;</div>
-                                <h2 className='m-5'> Gráfico alcista de bitcoin podría señalar disparada hacia los US$40.000</h2>
-                                <font size='5'>
-                                    <p className="card-text m-5" size='5' align="justify">Últimamente se ha presentado una gran tendencia alcista en el valor del bitcoin, luego de la gran caída de un precio de 60.000 euros a
+                        <img 
+                            className='img-fluid' 
+                            src='https://estaticos-cdn.elperiodico.com/clip/5d0a3ba8-43db-4b2f-abc9-882d8344e5d5_alta-libre-aspect-ratio_default_0.jpg' 
+                            alt='...'>
+                        </img>
+                        <div>
+                            <div className='m-4' align="justify">
+                                <font face='Britannic Bold'>
+                                    <h2 className='m-4'> Gráfico alcista de bitcoin podría señalar disparada hacia los US$40.000</h2>
+                                    <p className="card-text m-4 h5" size='5'>Últimamente se ha presentado una gran tendencia alcista en el valor del bitcoin, luego de la gran caída de un precio de 60.000 euros a
                                         aproximadamente 30.000 euros la unidad de bitcoin. Se recomienda a los usuarios tomar esta tendencia alcista con precaución debido a la volatilidad
                                         de la moneda.</p>
-                                    <p className='m-5'><b>Cordialmente - La organización -</b></p>
+                                    <p className='m-4 h5'><b>Cordialmente - La organización -</b></p>
                                 </font>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <div className="card bg-dark text-black mt-4 text-end">
-                        <img className='card-img-top' height={700} src='https://estudiotarazona.com/wp-content/uploads/2020/11/81cd82ce-2d69-417f-9c7a-324660761590-1000x500.jpg' alt='Aqui va una imagen'></img>
-                        <div className="card-img-overlay">
-                            <div className='m-5'>
-                                <font size='7' face='Britannic Bold'>
-                                    <p className='m-5'><b>CAMBIA TUS SOLES A BITCOINS EN NUESTRA CASA DE CAMBIO</b></p>
+                    {/* PROPUESTAS */}
+                    <div className="card bg-dark text-white mt-4">
+                        <img 
+                            className='img-fluid' 
+                            src='https://estudiotarazona.com/wp-content/uploads/2020/11/81cd82ce-2d69-417f-9c7a-324660761590-1000x500.jpg' 
+                            alt='...'>
+                        </img>
+                        <div>
+                            <div className='m-5' align="justify">
+                                <font face='Britannic Bold'>
+                                    <h2 className='m-4'>CAMBIA TUS SOLES A BITCOINS EN NUESTRA CASA DE CAMBIO</h2>
                                 </font>
                             </div>
                         </div>
