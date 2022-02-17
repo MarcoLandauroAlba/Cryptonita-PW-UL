1b23653664994f962e1d415300c7cdf8b4b21519
Jean Pierre Soto <juegosdecompu12@hotmail.com>
2022-02-16 21:16:16 -0500

    Paginas Avanzadas en resgistros

---
+++ components/FormularioIniciarSesion.component.js
@@ -0,0 +1,20 @@
+export default function FormularioIniciarSesion() {
+    return <div className="row mt-4">
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+            <div className="card">
+                <div className="card-body">
+                    <h3>Login</h3>
+                    <form>
+                        <label className="mt-2">Correo:</label>
+                        <input type="text" className="form-control mt-2"></input>
+                        <label className="mt-2">Contraseña:</label>
+                        <input type="text" className="form-control mt-2"></input>
+                        <button type="button" className="btn btn-primary mt-4">Ingresar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-4"></div>
+    </div>
+}
--- components/menuNavegacionAdminRegistrado.component.js
+++ components/menuNavegacionAdminRegistrado.component.js
@@ -1,22 +1,24 @@
-
-const MenuNavegacion = () => {
+const MenuNavegacionadminregistrado = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Cryptonita</a>
+                <a className="navbar-brand" href="/AdminRegistrado">Cryptonita</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Compra/Venta</a>
+                            <a className="nav-link active" aria-current="page" href="/ClientesPage">Clientes</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/procesoRegistro1">Abrir Cuenta</a>
+                            <a className="nav-link" href="/procesoRegistro1">Tipo de Cambio</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/IniciarSesion">Iniciar Sesion</a>
+                            <a className="nav-link" href="#">Operaciones</a>
+                        </li>
+                        <li>
+                            <button type="button" className="btn btn-danger">Cerrar Sesión</button>
                         </li>
                     </ul>
                 </div>
@@ -25,4 +27,4 @@ const MenuNavegacion = () => {
     )
 }
 
-export default MenuNavegacion
+export default MenuNavegacionadminregistrado
