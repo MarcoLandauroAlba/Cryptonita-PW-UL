ca8dcec28160af363ca178f2caa20a5a92dc5670
SeRgIo3004 <sergio3004.2002@gmail.com>
2022-02-17 08:12:14 -0500

    Nuevo carrusel

--- pages/AdminRegistrado.js
+++ pages/AdminRegistrado.js
@@ -1,6 +1,6 @@
 import MenuNavegacionadminregistrado from "../components/menuNavegacionAdminRegistrado.component";
 import Footer from "../components/footer.component";
-import Carrusel from "../components/Carrusel.component";
+import Carrusel from "../components/carrusel.component";
 
 export default function AdminRegistradoPage() {
     return <div>
