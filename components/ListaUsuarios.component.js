df58664b62135c5963ce66e035242a58469c5640
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2022-02-26 15:20:36 -0500

    Pages trabajadas y comentadas: ClientesPage
    Components trabajados y comentados: OpcionesUsuarioAdmin, ListaUsuario,ValidadoCambio,FormularioPR2 (una linea). Se necesita la base de datos para terminar todas las funcionalidades necesarias. Falta arreglar componente Validado Cambio Usuario. Se implemento la seguridad necesaria para estas paginas

--- components/ListaUsuarios.component.js
+++ components/ListaUsuarios.component.js
@@ -1,36 +1,50 @@
+/*
+props Utilizados:
+- tipoDeCliente: Solo renderiza en pantalla si es un cliente de tipo administrador (SEGURIDAD)
+- lista: Lleva la lista de usuarios que cumplen con los requerimientos enviados
+*/
 export default function ListaUsuarios(props) {
-    return <div>
-        <h3>Listado de Usuarios</h3>
-        <div className="table-responsive">
-            <table className="table mt-4">
-                <thead>
-                    <tr>
-                        <th>Número</th>
-                        <th>ID</th>
-                        <th>Nombre Completo</th>
-                        <th>DNI</th>
-                        <th>Correo Electrónico</th>
-                        <th>Número Teléfonico</th>
-                        <th>Estado</th>
-                        <th>Opciones</th>
-                    </tr>
-                </thead>
-                {
-                    props.lista.map((usuario) => {
-                        return <tr key={usuario.id}>
-                            <td>{usuario.numero}</td>
-                            <td>{usuario.id}</td>
-                            <td>{usuario.nombre}</td>
-                            <td>{usuario.dni}</td>
-                            <td>{usuario.correo}</td>
-                            <td>{usuario.numerotelf}</td>
-                            <td>{usuario.estado}</td>
-                            <button type="button" className="btn btn-link">Modificar</button>
-                            <button type="button" className="btn btn-link">Eliminar</button>
+
+    if (props.tipoDeCliente == 1) {
+        return <div>
+            <h3>Listado de Usuarios</h3>
+            <div className="table-responsive">
+                <table className="table mt-4">
+                    <thead>
+                        <tr>
+                            <th>Número</th>
+                            <th>ID</th>
+                            <th>Nombre Completo</th>
+                            <th>DNI</th>
+                            <th>Correo Electrónico</th>
+                            <th>Número Teléfonico</th>
+                            <th>Estado</th>
+                            <th>Opciones</th>
                         </tr>
-                    })
-                }
-            </table>
+                    </thead>
+                    <tbody>
+                        {
+                            props.lista.map((usuario) => {
+                                return <tr key={usuario.id}>
+                                    <td>{usuario.numero}</td>
+                                    <td>{usuario.id}</td>
+                                    <td>{usuario.nombre}</td>
+                                    <td>{usuario.dni}</td>
+                                    <td>{usuario.correo}</td>
+                                    <td>{usuario.numerotelf}</td>
+                                    <td>{usuario.estado}</td>
+                                    <td><button type="button" className="btn btn-link">Modificar</button>
+                                        <button type="button" className="btn btn-link">Eliminar</button></td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
+    }else{
+        return <div className="h1 text-center text-danger">
+            Usted no tiene permiso de estar en esta pagina
+        </div>
+    }
 }
--- components/OpcionesUsuariosAdmin.component.js
+++ components/OpcionesUsuariosAdmin.component.js
@@ -1,15 +1,124 @@
-const OpcionesUsuariosAdmin = () => {
-    return <div className="row mt-4">
-        <div className="col-md-4"></div>
-        <div className="col-md-8">
-            <p>Filtrar Usuarios
-                &nbsp;
-                <input type="text" placeholder="Texto a filtrar"></input>
-                &nbsp;
-                <button type="button" className="btn btn-primary">Buscar</button>
-            </p>
-        </div>
-    </div>
+import { useState } from "react"
+/*
+props Utilizados:
+- tipoDeCliente: Solo renderiza en pantalla si es un cliente de tipo administrador (SEGURIDAD)
+- buscarUsuarios: Sirve para poder buscar usuarios por medio de caracteristicas definidas en los inputs
+*/
+const OpcionesUsuariosAdmin = (props) => {
+
+    const [datosDNI, setDatosDNI] = useState()
+    const [datosNOMBRE, setDatosNOMBRE] = useState()
+    const [datosAPELLIDO, setDatosAPELLIDO] = useState()
+    const [datosCORREO, setDatosCORREO] = useState()
+
+    const setDatosOnChangeDNI = (event) => {
+        setDatosDNI(event.target.value)
+    }
+    const setDatosOnChangeNOMBRE = (event) => {
+        setDatosNOMBRE(event.target.value)
+    }
+    const setDatosOnChangeAPELLIDO = (event) => {
+        setDatosAPELLIDO(event.target.value)
+    }
+    const setDatosOnChangeCORREO = (event) => {
+        setDatosCORREO(event.target.value)
+    }
+
+    if (props.tipoDeCliente == 1) {                     // SOLO SE PODRA MOSTRAR ESTE CONTENIDO SI ES TIPO DE USUARIO ADMINISTRADOR (tipoDeCliente = 1)
+        return (
+            <div className="row my-4">
+                <div className="col-md-1"></div>
+                <div className="col-md-10 row">
+                    <div className="col">
+                        <form>
+                            <label className="mx-2 h5" >Filtrar por DNI:</label>
+                            <div className="d-flex justify-content-end">
+                                <input
+                                    className="mx-2"
+                                    type="number"
+                                    name="primero"
+                                    placeholder="12345678"
+                                    onChange={setDatosOnChangeDNI}
+                                    defaultValue={datosDNI}
+                                />
+                                <button
+                                    type="button"
+                                    className="mx-2 btn btn-primary"
+                                    onClick={() => { props.buscarUsuarios(datosDNI, 'DNI') }}>
+                                    Buscar
+                                </button>
+                            </div>
+                        </form>
+                        <form>
+                            <label className="mx-2 h5" >Filtrar por nombre:</label>
+                            <div className="d-flex justify-content-end">
+                                <input
+                                    className="mx-2"
+                                    type="text"
+                                    placeholder="Nombre"
+                                    name="segundo"
+                                    onChange={setDatosOnChangeNOMBRE}
+                                    defaultValue={datosNOMBRE}
+                                />
+                                <button
+                                    type="button"
+                                    className="mx-2 btn btn-primary"
+                                    onClick={() => { props.buscarUsuarios(datosNOMBRE, 'NOMBRE') }}>
+                                    Buscar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col">
+                        <form>
+                            <label className="mx-2 h5" >Filtrar por apellido:</label>
+                            <div className="d-flex justify-content-end">
+                                <input
+                                    className="mx-2"
+                                    type="text"
+                                    placeholder="Apellido"
+                                    name="tercero"
+                                    onChange={setDatosOnChangeAPELLIDO}
+                                    defaultValue={datosAPELLIDO}
+                                />
+                                <button
+                                    type="button"
+                                    className="mx-2 btn btn-primary"
+                                    onClick={() => { props.buscarUsuarios(datosAPELLIDO, 'APELLIDO') }}>
+                                    Buscar
+                                </button>
+                            </div>
+                        </form>
+                        <form>
+                            <label className="mx-2 h5" >Filtrar por correo:</label>
+                            <div className="d-flex justify-content-end">
+                                <input
+                                    className="mx-2"
+                                    type="email"
+                                    placeholder="ejemplo@correo.com"
+                                    name="cuarto"
+                                    onChange={setDatosOnChangeCORREO}
+                                    defaultValue={datosCORREO}
+                                />
+                                <button
+                                    type="button"
+                                    className="mx-2 btn btn-primary"
+                                    onClick={() => { props.buscarUsuarios(datosCORREO, 'CORREO') }}>
+                                    Buscar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="col-md-1"></div>
+            </div>
+        )
+
+
+    } else {
+        console.log('USTED NO ES ADMINISTRADOR')
+        return <></>
+    }
 }
 
 export default OpcionesUsuariosAdmin
