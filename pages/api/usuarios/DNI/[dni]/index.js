7f408fd23a2e2e018cffd13dfa2e4943c5bef3c4
Jean Pierre Soto <juegosdecompu12@hotmail.com>
2022-03-02 11:29:13 -0500

    Correcion en filtrado para mostrar estados

--- pages/api/usuarios/DNI/[dni]/index.js
+++ pages/api/usuarios/DNI/[dni]/index.js
@@ -6,9 +6,30 @@ const ClientesDNIHandler = async (req, res) => {
         const data = req.query
         console.log(data)
         const cliente = await obtenerClientexDNI(data.dni)
+        const clientesconEstado = []
+        for(let cl of cliente){
+            let estado = ""
+            if(cl.estado == true){
+                estado = "Validado"
+            }
+            else{
+                estado = "Por Validar"
+            }
+            clientesconEstado.push({
+                id: cl.id,
+                nombre: cl.nombre,
+                apellido: cl.apellido,
+                dni: cl.dni,
+                correo: cl.correo,
+                telefono: cl.telefono,
+                estado: estado,
+                createdAt: cl.createdAt,
+                updatedAt: cl.updatedAt
+            })
+        }
         res.json({
             msg: "",
-            cliente : cliente
+            cliente : clientesconEstado
         })
     }
 }
