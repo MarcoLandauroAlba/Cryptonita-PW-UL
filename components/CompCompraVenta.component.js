7d2a4387f70823d1cc6409118601fb86e8aeb2b9
Marco Antonio Landauro Alba <20191063@aloe.ulima.edu.pe>
2022-03-03 01:38:18 -0500

    Antes CompraVenta

--- components/CompCompraVenta.component.js
+++ components/CompCompraVenta.component.js
@@ -2,10 +2,10 @@ import { Image } from "react-bootstrap"
 import { useState } from 'react'
 
 const CompCompraVenta = (props) => {
-    const[cComp,setCComp]= useState()
-    const[cVent,setCVent]= useState()
+    const [cComp, setCComp] = useState()
+    const [cVent, setCVent] = useState()
     //const[calcC,setCalcC]= useState()
-    
+
     const setCCompOnChange = (event) => {
         setCComp(parseFloat(event.target.value))
     }
@@ -26,52 +26,52 @@ const CompCompraVenta = (props) => {
                 <div className="card text-center"> <br />
                     <h5 className="card-tittle">Precio Compra BTC</h5>
                     <div className="card">
-                    {
-                    (() => {
-                        return "S/."+ ((parseFloat(props.valor))*3.75).toFixed(4) + " = 1 BTC" 
-                    })()
-                    }
+                        {
+                            (() => {
+                                return "S/." + ((parseFloat(props.valor)) * 3.75).toFixed(4) + " = 1 BTC"
+                            })()
+                        }
 
                     </div><br />
                     <div className="card">
-                    Ingrese cantidad de BTC a comprar para realizar el calculo:
-                    <input type="text" className="form-control mt-2" id="pCompra" defaultValue={cComp} onChange={setCCompOnChange} />
+                        Ingrese cantidad de BTC a comprar para realizar el calculo:
+                        <input type="text" className="form-control mt-2" id="pCompra" defaultValue={cComp} onChange={setCCompOnChange} />
                         <div>
-                        {
-                        (() => {
-                        return  <input type="text" className="form-control mt-2" id="calcCompra" defaultValue={"S/."+parseFloat(cComp)*((parseFloat(props.valor))*3.75).toFixed(4)} />
-                        })()
-                         }    
+                            {
+                                (() => {
+                                    return <input type="text" className="form-control mt-2" id="calcCompra" defaultValue={"S/." + parseFloat(cComp) * ((parseFloat(props.valor)) * 3.75).toFixed(4)} />
+                                })()
+                            }
                         </div>
-                    
-                    </div><br/>
-                    <h5 className="card-tittle">Precio Venta BTC</h5> 
+
+                    </div><br />
+                    <h5 className="card-tittle">Precio Venta BTC</h5>
                     <div className="card">
-                    {
-                    (() => {
-                        return "1 BTC ="+ "S/."+ (parseFloat(props.valor)*(1+0.005)*3.75).toFixed(4)
-                    })()
-                    }  
-                    </div> 
-                    
-                    <br/>
-                    <div className="card">
-                    Ingrese cantidad de BTC a vender para realizar el calculo:
-                    <input type="text" className="form-control mt-2" id="pCompra" defaultValue={cVent} onChange={setCVentOnChange} />
-                    <div>
                         {
-                        (() => {
-                        return  <input type="text" className="form-control mt-2" id="calcCompra" defaultValue={"S/."+ parseFloat(cVent)*(parseFloat(props.valor)*(1+0.005)*3.75).toFixed(4)} />
-                        })()
-                         }    
+                            (() => {
+                                return "1 BTC =" + "S/." + (parseFloat(props.valor) * (1 + 0.005) * 3.75).toFixed(4)
+                            })()
+                        }
+                    </div>
+
+                    <br />
+                    <div className="card">
+                        Ingrese cantidad de BTC a vender para realizar el calculo:
+                        <input type="text" className="form-control mt-2" id="pCompra" defaultValue={cVent} onChange={setCVentOnChange} />
+                        <div>
+                            {
+                                (() => {
+                                    return <input type="text" className="form-control mt-2" id="calcCompra" defaultValue={"S/." + parseFloat(cVent) * (parseFloat(props.valor) * (1 + 0.005) * 3.75).toFixed(4)} />
+                                })()
+                            }
                         </div>
 
                     </div>
-                    *Los precios se actualizan cada 30s <br/>
-                    *El tipo de cambio utilizado es $1 = S/.3.75  
-                    <button className="btn btn-primary md-2"> Realizar Compra</button><br/>
+                    *Los precios se actualizan cada 30s <br />
+                    *El tipo de cambio utilizado es $1 = S/.3.75
+                    <button className="btn btn-primary md-2"> Realizar Compra</button><br />
                     <button className="btn btn-primary md-2"> Realizar Venta</button>
-                    <br/>
+                    <br />
                 </div>
             </div>
         </div>)
